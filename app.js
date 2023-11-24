8e258f141ed18f102e1f8429a64f20ea558353c2
Christian Ek <hi@christianek.io>
2023-11-24 15:18:55 +0100

    Add on_off and thermostat_program

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const Homey = require('homey');
 class EbecoApp extends Homey.App {
 
   onInit() {
+    this.registerFlowListeners();
+
     this.log('Successfully init Ebeco Connect App');
+  }
+
+  registerFlowListeners() {
+    // condition cards
+    this.homey.flow.getConditionCard('thermostat_program_equals')
+      .registerRunListener(async args => {
+        return (args.device.getCapabilityValue('thermostat_program') === args.mode);
+      });
   }
 
 }
